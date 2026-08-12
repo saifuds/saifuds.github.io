80e51a5225dc5961d934e2f25d34ffc854f7c690
Suemayya Saifuddin <saifuds@mcmaster.ca>
2026-08-12 16:57:13 -0400

    fix snake game page

--- js/snake-game.js
+++ js/snake-game.js
@@ -443,7 +443,7 @@ cancelClose?.addEventListener("click", () => {
 });
 
 confirmClose?.addEventListener("click", () => {
-  window.location.href = "/";
+  window.location.href = "/#snake-project";
 });
 
 minimizeButton?.addEventListener("click", () => {
